d1637298076713ba212427b064ffd537f9160f52
Adriel Baez <baez.adriel27@gmail.com>
2021-04-25 21:53:39 -0300

    cambios 25/04

--- frontend/src/components/CardItinerary.js
+++ frontend/src/components/CardItinerary.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Collapse, Button } from 'reactstrap';
 
-const CardItinerary = ({itinerary}) => {
+const CardItinerary = ({ itinerary }) => {
     const [isOpen, setIsOpen] = useState(false);
     const [changeNameBtn, setChangeNameBtn] = useState('View More')
 
@@ -17,28 +17,27 @@ const CardItinerary = ({itinerary}) => {
     return (
         <div>
             <div className="container-itinerary">
-                <h2>{itinerary.title}</h2>
-                <div className="img-itinerary" style={{ backgroundImage: `url('/assets/${itinerary.authorPicture}')` }}></div>
-                <h3>{itinerary.authorName}</h3>
-                <div className="itinerary-details">
-                    <p><span>Price:</span>{'💵'.repeat(itinerary.price)}</p>
-                    <p><span>Duration:</span> {itinerary.duration} hours</p>
-                    <p><i className="far fa-heart heart-icon"></i> {itinerary.likes}</p>
-                </div>
-                <div className="hashtags">
-                    {itinerary.hashtags.map((hashtag, index) => {
-                        return (<p key={index}>{hashtag}</p>)
-                    })}
+                    <h2>{itinerary.title}</h2>
+                    <div className="img-itinerary" style={{ backgroundImage: `url('/assets/${itinerary.authorPicture}')` }}></div>
+                    <h3>{itinerary.authorName}</h3>
+                    <div className="itinerary-details">
+                        <p><span>Price:</span>{'💵'.repeat(itinerary.price)}</p>
+                        <p><span>Duration:</span> {itinerary.duration} hours</p>
+                    </div>
+                        <p className="likes"><i className="far fa-heart heart-icon"></i> {itinerary.likes}</p>
+                    <div className="hashtags">
+                        {itinerary.hashtags.map((hashtag, index) => {
+                            return (<p key={index}>{hashtag}</p>)
+                        })}
 
-                </div>
+                    </div>
                 <Collapse isOpen={isOpen}>
-                        <div className="container-itinerary-small">
-                            <h2>Under Construction</h2>
-                        </div>
+                    <div className="container-img-contruction" style={{ backgroundImage: `url('/assets/underconstruction.png')` }}>
+                    </div>
                 </Collapse>
                 <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>{changeNameBtn}</Button>
             </div>
-        </div>    
-  );
+        </div>
+    );
 }
 export default CardItinerary
