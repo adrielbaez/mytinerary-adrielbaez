ff2fd58f69ec33790a6da7b1cef4421d14f5e49d
Adriel Baez <baez.adriel27@gmail.com>
2021-05-17 22:53:06 -0300

    changes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const router = require('./routes/index');
 require('./config/database');
+const path = require('path')
 const app = express(); 
 require('./config/passport')
 
@@ -11,6 +12,18 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', router);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname+"/client/build/index.html"))
+    })
+}
+
+const host = process.env.HOST || '0.0.0.0'
+const port = process.env.PORT 
+
+app.listen(port, host, () => console.log("App listening on port "+port+" on "+host))
 
 app.listen(4000, () => console.log('App listening on port 4000'));
 
